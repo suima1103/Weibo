dcebf1af926719653e52631a8e4ea5fbd0058f79
袁泉亮 <yensenryou@gmail.com>
2017-11-05 11:02:21 +0900

    文件结构部署，设置启动文件

---
+++ chuanzhiWeibo/Classes/View(视图和控制器)/Main/WBMainViewController.swift
@@ -0,0 +1,22 @@
+//
+//  WBMainViewController.swift
+//  chuanzhiWeibo
+//
+//  Created by 袁泉亮 on 2017/11/04.
+//  Copyright © 2017年 袁泉亮. All rights reserved.
+//
+
+import UIKit
+
+class WBMainViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+        
+    }
+
+
+
+}
