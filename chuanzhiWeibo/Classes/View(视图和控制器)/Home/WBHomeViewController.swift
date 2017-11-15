4a53b2f75812a66ca877c05c0ea5f5abae116f7b
袁泉亮 <yensenryou@gmail.com>
2017-11-15 23:07:34 +0900

    添加测试控制器,push显示

--- chuanzhiWeibo/Classes/View(视图和控制器)/Home/WBHomeViewController.swift
+++ chuanzhiWeibo/Classes/View(视图和控制器)/Home/WBHomeViewController.swift
@@ -13,23 +13,31 @@ class WBHomeViewController: WBBaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+       
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // 显示好友
+    @objc private func showFriends() {
+        print(#function)
+        
+        let vc = WBDemoViewController()
+        
+        vc.hidesBottomBarWhenPushed = true 
+        
+        navigationController?.pushViewController(vc, animated: true)
     }
-    */
+}
 
+// MARK: - 设置界面
+extension WBHomeViewController {
+    
+    override func setUI() {
+        
+        super.setUI()
+        
+        print("home")
+        
+        // 设置导航栏按钮
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "好友", style: .plain, target: self, action: #selector(showFriends))
+    }
 }
